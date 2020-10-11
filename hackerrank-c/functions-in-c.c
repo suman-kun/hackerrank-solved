80f3bfc8d0134afd7ef3f8d0162ddf25f4749fbd
suman-kun <suman.mandal1020@gmail.com>
2020-10-11 20:38:23 +0530

    simple function qs

---
+++ hackerrank-c/functions-in-c.c
@@ -0,0 +1,53 @@
+/*
+Task Title: Functions in C
+
+Task:   Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
+
+Note:   There is not built in max function in C. Code that will be reused is often put in a separate function, e.g. int max(x, y) that returns the greater of the two values.
+
+Input Format:   Input will contain four integers -a,b,c,d, one on each line.
+
+Output Format:  Print the greatest of the four integers.
+Note:   I/O will be automatically handled.
+
+.
+
+Sample Input
+
+3
+4
+6
+5
+
+Sample Output
+
+6
+
+.
+
+*/
+
+#include <stdio.h>
+ 
+int max_of_four(int a,int b,int c,int d);
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+int max_of_four(int a,int b,int c,int d){
+    int ans;
+    //checking all integers against each other
+    if(a>b && a>c && a>d)
+        return a;
+    else if(b>a && b>c && b>d)
+        return b;
+    else if(c>a && c>b && c>d)
+        return c;
+    else 
+        return d;
+}
